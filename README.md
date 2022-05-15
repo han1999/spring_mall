5b89fcbb70167da6e91f50c508b7a94b6bb65806
Han Xiao <1043599451@qq.com>
2022-05-15 16:25:51 +0800

    Start developing an online mall admin system by SpringBoot

---
+++ README.md
@@ -0,0 +1,6 @@
+# spring_mall
+## An online mall admin system developed by SpringBoot
+
+start time: 2022.05
+
+end time: 2022.
---
+++ src/main/java/com/hanxiao/spring_mall/SpringMallApplication.java
@@ -0,0 +1,13 @@
+package com.hanxiao.spring_mall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMallApplication.class, args);
+    }
+
+}
